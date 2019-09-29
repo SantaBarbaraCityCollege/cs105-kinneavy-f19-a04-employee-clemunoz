86d681dc8c4f41459388a0dc82e1bea432060d7b
clemunoz <clmunoz@live.com>
2019-09-29 15:35:57 -0700

    Removing brackets from my name/crn

--- src/main/java/edu/sbcc/cs105/Program.java
+++ src/main/java/edu/sbcc/cs105/Program.java
@@ -1,12 +1,12 @@
 /**
  * CS 105 Theory and Practice I
- * CRN: [38065]
+ * CRN: 38065
  * Assignment: Employee
  * 
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
  * 
- * @author [Clemente Munoz]
+ * @author Clemente Munoz
  */
 package edu.sbcc.cs105;
 
